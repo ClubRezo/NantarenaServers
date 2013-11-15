e3ac3dc048d4fa896b7b532e65ad1a2ad1fed681
KenTiN <quentin.lebourgeois@gmail.com>
2013-11-15 15:25:50 +0100

    Modification du script pour les serveurs dédiées

--- dedicated/dedicated.py
+++ dedicated/dedicated.py
@@ -10,7 +10,7 @@ Handle Valve Dedicated Server launching, able to launch several servers in paral
 @date:    November 2013
 """
 
-import sys, argparse
+import os, sys, argparse, time, importlib
 
 # program information
 __program__ = "dedicated"
@@ -18,6 +18,7 @@ __version__ = "1 (dev)"
 __description__ = "Valve Dedicated Server Launcher"
 
 # default parameters
+DE_SCRIPTS_DIR = "/home/nantarena/servers/dedicated"
 DE_DEFAULT_GAME = "csgo"
 DE_INSTANCES = 1
 
@@ -41,17 +42,51 @@ class LauncherArgumentParser(argparse.ArgumentParser):
         # command line description (version number given too)
         self.description = "%s Version %s" % (__description__, __version__)
         
-        self.add_argument("game", choices=["csgo"],
-            help="game to launch", default="casual")
+        self.add_argument("game", choices=["csgo"], help="game to launch", default="casual")
         self.add_argument("-N", "--instances", default=DE_INSTANCES, type=int, dest="de_instances", 
             help="number of instances (default: %i)" % DE_INSTANCES)
+        self.add_argument("-O", "--options", dest="de_options", nargs=argparse.REMAINDER,
+            help="server options")
 
-def launch_csgo(options):
+def launch(args):
     """
-    Launch CSGO server with options
+    Launch a dedicated server with options
     """
 
+    # goto the scripts dir
+    os.chdir(DE_SCRIPTS_DIR)
 
+    # automatic -d option append
+    args.de_options.append("--daemon")
+
+    servers_list = []
+    port = 27015
+
+    if args.game == "csgo":
+        for i in range(0, args.de_instances):
+            # specify a custom screen name
+            args.de_options.extend(["--screen-name", "%s.%i" % (args.game, port)])
+            # append server to servers list
+            servers_list.append("%s:%i" % (args.game, port))
+
+            launch_csgo(args, port)
+            port = port + 10 ; time.sleep(2)
+
+    return servers_list
+
+def launch_csgo(args, port):
+    """
+    Launch CSGO Dedicated Server as a daemon
+    """
+
+    # force port
+    args.de_options.extend(["-p", port])
+
+    # launch csgo ds
+    result = os.system("python %s.py %s" % (args.game, " ".join(str(x) for x in args.de_options)))
+
+    if result != 0:
+        raise Exception("Unable to launch dedicated server on port %i" % port)
 
 def main():
     """
@@ -61,9 +96,17 @@ def main():
     args = LauncherArgumentParser().parse_args()
 
     try:
-        start_server(args)
+        servers_list = launch(args)
+
+        # clear console
+        os.system("clear")
+
+        for server in servers_list:
+            print "[ " + bcolors.OKGREEN + "OK" + bcolors.ENDC + " ] " + "Launched 1 server %s" % (server)
     except Exception as e:
-        print "[" + bcolors.FAIL + "fail" + bcolors.ENDC  + "] " + "Error while launching dedicated server" + " (error: %s)" % e ; return 1
+        print "[" + bcolors.FAIL + "fail" + bcolors.ENDC + "] " + "Error while launching dedicated server" + " (error: %s)" % e ; return 1
+
+    print "[" + bcolors.BOLD + "info" + bcolors.ENDC + "] " + "Launched %i server(s) of %s" % (args.de_instances, args.game)
 
     return 0
 
